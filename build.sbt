8d873071fa687b95061c44c3d542b98aca521467
Raul Garcia <raulantoniogl92@gmail.com>
2021-11-04 13:36:25 -0600

    * config for http server with htt4s

--- build.sbt
+++ build.sbt
@@ -4,10 +4,38 @@ version := "0.1"
 
 scalaVersion := "2.13.3"
 
-libraryDependencies ++= Seq(
-  "io.circe"      %% "circe-core"   % "0.12.3",
-  "io.circe"      %% "circe-parser" % "0.12.3",
-  "org.tpolecat"  %% "doobie-core"  % "0.8.8",
-  "org.scalatest" %% "scalatest"    % "3.2.0" % "test"
 
+
+lazy val catsEffectVersion    = "2.1.3"
+lazy val catsVersion          = "2.1.1"
+lazy val circeVersion         = "0.13.0"
+lazy val doobieVersion        = "0.9.0"
+lazy val fs2Version           = "2.3.0"
+lazy val kindProjectorVersion = "0.9.10"
+lazy val log4catsVersion      = "1.1.1"
+lazy val sangriaCirceVersion  = "1.3.0"
+lazy val sangriaVersion       = "2.1.3"
+lazy val scala12Version       = "2.12.11"
+lazy val http4sVersion        = "0.21.4"
+lazy val slf4jVersion         = "1.7.32"
+
+
+libraryDependencies ++= Seq(
+
+  "org.typelevel"        %% "cats-core"           % catsVersion,
+  "org.typelevel"        %% "cats-effect"         % catsEffectVersion,
+  "co.fs2"               %% "fs2-core"            % fs2Version,
+  "co.fs2"               %% "fs2-io"              % fs2Version,
+  "org.sangria-graphql"  %% "sangria"             % sangriaVersion,
+  "org.sangria-graphql"  %% "sangria-circe"       % sangriaCirceVersion,
+  "org.tpolecat"         %% "doobie-core"         % doobieVersion,
+  "org.tpolecat"         %% "doobie-postgres"     % doobieVersion,
+  "org.tpolecat"         %% "doobie-hikari"       % doobieVersion,
+  "org.http4s"           %% "http4s-dsl"          % http4sVersion,
+  "org.http4s"           %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s"           %% "http4s-circe"        % http4sVersion,
+  "io.circe"             %% "circe-optics"        % circeVersion,
+  "io.chrisdavenport"    %% "log4cats-slf4j"      % log4catsVersion,
+  "org.slf4j"            %  "slf4j-simple"        % slf4jVersion,
+  "org.scalatest" %% "scalatest"    % "3.2.0" % "test"
 )
---
+++ src/main/scala/com/arkondata/training/Main.scala
@@ -0,0 +1,102 @@
+package com.arkondata.training
+
+import _root_.sangria.schema._
+import cats.effect._
+import cats.implicits._
+import com.arkondata.training.repo.MasterRepo
+import com.arkondata.training.sangria.SangriaGraphQL
+import com.arkondata.training.schema.{MutationType, QueryType, WorldDeferredResolver}
+import doobie._
+import doobie.hikari._
+import doobie.util.ExecutionContexts
+import io.chrisdavenport.log4cats.Logger
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import org.http4s._
+import org.http4s.dsl._
+import org.http4s.headers.Location
+import org.http4s.implicits._
+import org.http4s.server.Server
+import org.http4s.server.blaze.BlazeServerBuilder
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.global
+
+object Main extends  IOApp {
+
+
+  // Construct a transactor for connecting to the database.
+  def transactor[F[_]: Async: ContextShift](
+                                             blocker: Blocker
+                                           ): Resource[F, HikariTransactor[F]] =
+    ExecutionContexts.fixedThreadPool[F](10).flatMap { ce =>
+      HikariTransactor.newHikariTransactor(
+        "org.postgresql.Driver",
+        "jdbc:postgresql:training_inegi",
+        "postgres",
+        "luaragl",
+        ce,
+        blocker
+      )
+    }
+
+  // Construct a GraphQL implementation based on our Sangria definitions.
+  def graphQL[F[_]: Effect: ContextShift: Logger](
+                                                   transactor:      Transactor[F],
+                                                   blockingContext: ExecutionContext
+                                                 ): GraphQL[F] =
+    SangriaGraphQL[F](
+      Schema(
+        query    = QueryType[F],
+//        mutation = Some(MutationType[F])
+      ),
+      WorldDeferredResolver[F],
+//      MasterRepo.fromTransactor(transactor).pure[F],
+      MasterRepo.fromTransactor(transactor).pure[F],
+      blockingContext
+    )
+
+  // Playground or else redirect to playground
+  def playgroundOrElse[F[_]: Sync: ContextShift](
+                                                  blocker: Blocker
+                                                ): HttpRoutes[F] = {
+    object dsl extends Http4sDsl[F]; import dsl._
+    HttpRoutes.of[F] {
+
+      case GET -> Root / "playground.html" =>
+        StaticFile
+          .fromResource[F]("/assets/playground.html", blocker)
+          .getOrElseF(NotFound())
+
+      case _ =>
+        PermanentRedirect(Location(Uri.uri("/playground.html")))
+
+    }
+  }
+
+  // Resource that mounts the given `routes` and starts a server.
+  def server[F[_]: ConcurrentEffect: ContextShift: Timer](
+                                                           routes: HttpRoutes[F]
+                                                         ): Resource[F, Server[F]] =
+    BlazeServerBuilder[F]( ExecutionContext.global )
+      .bindHttp(8080, "localhost")
+      .withHttpApp(routes.orNotFound)
+      .resource
+
+  // Resource that constructs our final server.
+  def resource[F[_]: ConcurrentEffect: ContextShift: Timer](
+                                                             implicit L: Logger[F]
+                                                           ): Resource[F, Server[F]] =
+    for {
+      b   <- Blocker[F]
+      xa  <- transactor[F](b)
+      gql  = graphQL[F](xa, b.blockingContext)
+      rts  = GraphQLRoutes[F](gql) <+> playgroundOrElse(b)
+      svr <- server[F](rts)
+    } yield svr
+
+  // Our entry point starts the server and blocks forever.
+  def run(args: List[String]): IO[ExitCode] = {
+    implicit val log = Slf4jLogger.getLogger[IO]
+    resource[IO].use(_ => IO.never.as(ExitCode.Success))
+  }
+}

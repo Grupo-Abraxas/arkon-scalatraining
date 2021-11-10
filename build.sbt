c11e86e3a8d4525a09175ccb080ff8105e209894
sergio garcía <sogarcia@grupoabraxas.com>
2021-11-10 15:35:31 -0600

    serialization succeded

--- build.sbt
+++ build.sbt
@@ -9,9 +9,14 @@ val http4sVersion = "0.22.7"
 libraryDependencies ++= Seq(
   "io.circe" %% "circe-core" % "0.12.3",
   "io.circe" %% "circe-parser" % "0.12.3",
+  "io.circe" %% "circe-generic" % "0.12.3",
+
   "org.tpolecat" %% "doobie-core" % "0.8.8",
   "org.scalatest" %% "scalatest" % "3.2.0" % "test",
+
   "org.http4s" %% "http4s-dsl" % http4sVersion,
   "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+  "org.http4s" %% "http4s-circe" % http4sVersion,
+
   "org.sangria-graphql" %% "sangria" % "2.0.0",
 )
--- src/main/scala/service/GraphqlService.scala
+++ src/main/scala/service/GraphqlService.scala
@@ -1,13 +1,19 @@
 package service
 
 import cats.effect.IO
+import domain.{Shop, ShopType}
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
+import io.circe.generic.auto._
+import io.circe.syntax.EncoderOps
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 
 object GraphqlService extends Http4sDsl[IO] {
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "data" => Ok("empty")
+    case GET -> Root / "data" => Ok(new ShopType(0, "retail").asJson)
+
+//    case req @ POST -> Root / "graphql" =>
   }
 
 }

fd1a3d101999ebf868dcaf9361e4bf4b88e772a8
sergio garcía <sogarcia@grupoabraxas.com>
2021-11-11 21:08:55 -0600

    schema definition

---
+++ src/main/scala/graphql/Schema.scala
@@ -0,0 +1,16 @@
+package graphql
+
+import domain.{Activity, Shop, ShopType, Stratum}
+import sangria.macros.derive.{deriveInputObjectType, deriveObjectType}
+
+object Schema {
+  val Activity = deriveObjectType[Unit, Activity]
+
+  val Stratum = deriveObjectType[Unit, Stratum]
+
+  val ShopType = deriveObjectType[Unit, ShopType]
+
+  val Shop = deriveObjectType[Unit, Shop]
+
+  val createShopInput = deriveInputObjectType[CreateShopInput]
+}

ffc5159de2a68dec50720bce89303e0feb058ed1
Raul Garcia <raulantoniogl92@gmail.com>
2021-11-16 11:34:25 -0600

    * is added circe for map a response from inegi

--- src/main/scala/com/arkondata/training/model/CreateShopInput.scala
+++ src/main/scala/com/arkondata/training/model/CreateShopInput.scala
@@ -1,5 +1,8 @@
 package com.arkondata.training.model
 
+
+import io.circe._
+
 case class CreateShopInput (
       id: Int,
       name: String,
@@ -14,3 +17,28 @@ case class CreateShopInput (
       lat: Double,
       long: Double
   )
+
+
+object InegiResponse {
+
+
+  implicit val decode2: Decoder[CreateShopInput] = (hCursor: HCursor) =>
+    for {
+
+      id <- hCursor.get[Int]( "Id" )
+      name <- hCursor.get[String]( "Nombre" )
+      businessName <- hCursor.get[String]( "Razon_social" )
+      activity <- hCursor.get[String]( "Clase_actividad" )
+      stratum <- hCursor.get[String]( "Estrato" )
+      address <- hCursor.get[String]( "Ubicacion" )
+      phoneNumber <- hCursor.get[String]( "Telefono" )
+      email <- hCursor.get[String]( "Correo_e" )
+      webSite <- hCursor.get[String]( "Sitio_internet" )
+      shopType <- hCursor.get[String]( "Tipo" )
+      latitud <- hCursor.get[Double]( "Longitud" )
+      longitud <- hCursor.get[Double]( "Latitud" )
+
+    } yield CreateShopInput(  id, name, businessName , activity , stratum  , address, phoneNumber, email, webSite, shopType, latitud, longitud  )
+
+
+}

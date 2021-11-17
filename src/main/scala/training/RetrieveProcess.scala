5b48db86d21959f6c8b20e6a8891a216a7547084
Raul Gonzalez <rauls.gh24@gmail.com>
2021-11-17 17:23:10 -0600

    feat: Resolve next issues - Initialize scrapper project - Retrieve data from INEGI - Execute createShop mutation from scrapper

---
+++ src/main/scala/training/RetrieveProcess.scala
@@ -0,0 +1,70 @@
+package training
+
+import cats.effect._
+import io.circe.generic.auto._
+import io.circe.literal._
+import org.http4s.Method.POST
+import org.http4s.Status.Successful
+import org.http4s.circe._
+import org.http4s.client.Client
+import org.http4s.client.blaze.BlazeClientBuilder
+import org.http4s.client.dsl.Http4sClientDsl
+import org.http4s.{Request, Uri}
+import training.inegi.Shop
+
+import scala.concurrent.ExecutionContext.global
+
+object RetrieveProcess extends IOApp with Http4sClientDsl[IO] {
+  def postToServer(client: Client[IO], shop: Shop): IO[String] = {
+    val request: IO[Request[IO]] = POST(
+      json"""
+        {
+          "query": "mutation CreateShop($$input, CreateShopInput) {createShop(input: $$input) {id}}",
+          "variables": {
+            "input": {
+              "id": "${shop.Id}",
+              "name": "${shop.Nombre}",
+              "businessName": "${shop.Razon_social}",
+              "activity": "${shop.Clase_actividad}",
+              "stratum": "${shop.Estrato}",
+              "address": "${shop.Calle + shop.Num_Exterior + shop.Colonia + shop.Ubicacion}",
+              "phoneNumber": "${shop.Telefono}",
+              "email": "${shop.Correo_e}",
+              "website": "${shop.Sitio_internet}",
+              "shopType": "${shop.Tipo}",
+              "lat": "${shop.Latitud}",
+              "long": "${shop.Longitud}"
+          }
+        }
+      """,
+      Uri.uri("http://127.0.0.1:8080/graphql")
+    )
+    client.expect[String](request)
+  }
+
+  def getDataINEGI(
+      client: Client[IO],
+      lat: Float,
+      lng: Float
+  ): IO[Unit] = IO {
+    val shops = client
+      .get(
+        Uri.uri(
+          s"https://www.inegi.org.mx/app/api/denue/v1/consulta/Buscar" +
+            s"/todos/$lat,$lng/5000/${System.getenv("INEGI_TOKEN")}"
+        )
+      ) { case Successful(resp) => resp.decodeJson[List[Shop]] }
+      .unsafeRunSync()
+
+    for (shop <- shops) {
+      postToServer(client, shop).unsafeRunSync()
+    }
+  }
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val List(lat, lng) = args
+    BlazeClientBuilder[IO](global).resource
+      .use(client => getDataINEGI(client, lat.toFloat, lng.toFloat))
+      .as(ExitCode.Success)
+  }
+}
---
+++ src/main/scala/training/inegi/Shop.scala
@@ -0,0 +1,26 @@
+package training.inegi
+
+final case class Shop(
+    CLEE: String,
+    CP: String,
+    Calle: String,
+    Clase_actividad: String,
+    Colonia: String,
+    Correo_e: String,
+    Estrato: String,
+    Id: String,
+    Latitud: String,
+    Longitud: String,
+    Nombre: String,
+    Num_Exterior: String,
+    Num_Interior: String,
+    Razon_social: String,
+    Sitio_internet: String,
+    Telefono: String,
+    Tipo: String,
+    Tipo_vialidad: String,
+    Ubicacion: String,
+    nom_corredor_industrial: String,
+    numero_local: String,
+    tipo_corredor_industrial: String
+)

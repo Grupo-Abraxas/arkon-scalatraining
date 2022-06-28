6e18089d2f0ce5adc57a306a6dc83e4e5014749a
Manuel Alejandro Bohorquez Medina <mbohorquez@grupoabraxas.com>
2022-06-28 14:11:37 -0500

    habilita ruta http4s  consumiendo BD  /estados

--- src/main/scala/controller/Main.scala
+++ src/main/scala/controller/Main.scala
@@ -1,22 +1,26 @@
 package controller
 
 import cats.data.Kleisli
+import cats.effect._
+import cats.implicits._
+import io.circe.Json
+import io.circe.generic.auto._
+import io.circe.syntax._
+import io.circe.{Decoder, Encoder}
 import cats.effect.{ExitCode, IO, IOApp}
 import doobie.util.transactor.Transactor
 import org.http4s.server.{Router, Server}
 import org.http4s.server.blaze._
-import org.http4s.dsl._
+import org.http4s.dsl.io._
 import org.http4s._
 import org.http4s.implicits._
-import cats.effect._
-import cats.implicits._
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import cats.effect._
-import com.comcast.ip4s._
+import graphql.SangriaGraphql
+import graphql.SangriaGraphql.estadoSchema
+import model.Estado
+import org.http4s.FormDataDecoder.formEntityDecoder
 import org.http4s.HttpRoutes
-import org.http4s.dsl.io._
-import org.http4s.implicits._
+import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
+import repository.EstadoRepo.findAllEstado
 
 object Main extends IOApp{
 
@@ -33,9 +37,13 @@ object Main extends IOApp{
   }.orNotFound
 
   val route: HttpRoutes[IO] = HttpRoutes.of[IO] {
+
     case GET -> Root / "length" / str => Ok(str.length.toString)
     case GET -> Root / "hello" / name => Ok(s"Hello, $name.")
+    case GET -> Root / "estados" => Ok(findAllEstado().asJson)
+
   }
+
 
   val app: Kleisli[IO, Request[IO], Response[IO]] = Router(
     "/" -> route
